896604d1e34ddd68817a5811ad490ee1602eb0fc
promm-dd <135016789+promm-dd@users.noreply.github.com>
2024-08-23 18:17:20 +0200

    update func 1

--- functions/func1.py
+++ functions/func1.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    input_date= datetime.strptime(date.strip(), "%Y-%m-%d").date()
+    try: 
+        input_date= datetime.strptime(date.strip(), "%Y-%m-%d").date()
+    except ValueError:
+        return "invalid date. Use 'YYYY-MM-DD' "
     current_date= datetime.today().date()
     delta= current_date - input_date
     return delta.days
@@ -9,3 +12,10 @@ def get_days_from_today(date):
 date_string = "2023-12-05"
 days_difference =get_days_from_today(date_string)
 print(days_difference)
+
+get_days_from_today("2021-10-09")
+
+
+date_string_invalid = "2021.10.09"
+days_invalid = get_days_from_today(date_string_invalid)
+print(days_invalid)
